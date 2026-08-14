9b710e376bbad74997417876c8572cc19cd8c3a2
Basry M. <basga.founder@gmail.com>
2026-08-14 09:11:05 +0700

    Fix GitHub Pages admin routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ProjectDetail from '../views/ProjectDetail.vue'
 import NotFound from '../views/NotFound.vue'
 import Admin from '../views/Admin.vue'
-export default createRouter({history:createWebHistory(),scrollBehavior:()=>({top:0}),routes:[{path:'/',component:Home},{path:'/admin',component:Admin},{path:'/projects/:slug',component:ProjectDetail},{path:'/:pathMatch(.*)*',component:NotFound}]})
+
+const useHashRouting = import.meta.env.BASE_URL !== '/'
+
+export default createRouter({
+  history: useHashRouting ? createWebHashHistory() : createWebHistory(),
+  scrollBehavior: () => ({ top: 0 }),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/admin', component: Admin },
+    { path: '/projects/:slug', component: ProjectDetail },
+    { path: '/:pathMatch(.*)*', component: NotFound }
+  ]
+})
